39cfe57d26f0624f86081ea94e6df5f27b3a01d0
TrucLuu <truclh@hotmail.com>
2015-06-22 16:19:24 +0700

    login-logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,24 @@ class User < ActiveRecord::Base
                                   foreign_key: "followed_id",
                                   dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
-  has_many :followers, through: :passive_relationships, source: :follower
-  
-  before_save {self.email = email.downcase}
-
-  validates :name, presence: true, length: {maximum: Settings.setting.maximum_length_name}
-  validates :email, presence: true, length: {maximum: Settings.setting.maximum_length_email},
-                    format: {with: Settings.setting.VALID_EMAIL_REGEX},
-                    uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: Settings.setting.minimum_length_password}
-
+  has_many :followers, through: :passive_relationships, source: :follower,
+                       dependent: :destroy
   has_secure_password
+
+  validates :name, presence: true, length: {maximum: Settings.length.short_maximum}
+  validates :email, presence: true, length: {maximum: Settings.length.long_maximum},
+                    format: {with: Settings.VALID_EMAIL_REGEX},
+                    uniqueness: {case_sensitive: false},
+                    if: "new_record?"
+  validates :password, length: {minimum: Settings.length.minimum}, if: "password_set?"
+
+  def password_set?
+    new_record? || password.present?
+  end
+
+  def User.digest value
+    cost = BCrypt::Engine.cost
+    cost = ActiveModel::SecurePassword.min_cost if BCrypt::Engine::MIN_COST
+    BCrypt::Password.create(value, cost: cost)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get "contact" => "static_pages#contact"
 
   resources :users
+
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
 end
