140b4ac9758f5bbfb02ae33f0bc54eec2dcd48ca
Do Tuan Minh <do.tuan.minh@gmail.com>
2015-06-23 16:46:01 +0700

    signup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622090733) do
+ActiveRecord::Schema.define(version: 20150624022443) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean  "correct"
