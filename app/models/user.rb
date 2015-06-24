fd52f1a2705f5deb380bf591fc4c81363742f594
Do Tuan Minh <do.tuan.minh@gmail.com>
2015-06-24 17:15:29 +0700

    remember

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   before_create :create_activation_digest
-
-  attr_accessor :activation_token, :reset_token
+  attr_accessor :activation_token, :reset_token, :remember_token
 
   has_many :lessons, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship",
@@ -13,7 +12,6 @@ class User < ActiveRecord::Base
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower,
                        dependent: :destroy
-  has_secure_password
 
   validates :name, presence: true, length: {maximum: Settings.length.short_maximum}
   validates :email, presence: true, length: {maximum: Settings.length.long_maximum},
@@ -21,6 +19,8 @@ class User < ActiveRecord::Base
                     uniqueness: {case_sensitive: false},
                     if: "new_record?"
   validates :password, length: {minimum: Settings.length.minimum}, if: "password_set?"
+
+  has_secure_password
 
   def password_set?
     new_record? || password.present?
@@ -34,7 +34,7 @@ class User < ActiveRecord::Base
 
   def User.digest string
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+    BCrypt::Engine.cost
     BCrypt::Password.create string, cost: cost
   end
 
@@ -46,14 +46,28 @@ class User < ActiveRecord::Base
     update_attributes active: true, activated_at: Time.zone.now
   end
 
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
   def authenticated? attribute, token
     digest = send "#{attribute}_digest"
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password? token
+    end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
   end
 
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
+  def forget
+    update_attributes remember_digest: nil
   end
 
   def create_activation_digest
