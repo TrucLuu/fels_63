0e56de15e5feb3e2e7bb5155013ff4a9bd471947
TrucLuu <truclh@hotmail.com>
2015-06-26 17:18:50 +0700

    Admin :: Word

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,4 +2,8 @@ class Word < ActiveRecord::Base
   has_many :results, dependent: :destroy
   has_many :answers, dependent: :destroy
   belongs_to :category
+
+  accepts_nested_attributes_for :answers,
+                                reject_if: lambda {|a| a[:content].blank?},
+                                allow_destroy: true
 end
