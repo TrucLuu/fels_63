fa5ca401efe813d5a391918851021c3e661f3968
michalplachyFramgia <michal.plachy@framgia.com>
2015-06-29 10:01:22 +0700

    User page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      @lessons = Lesson.follow_learned(current_user).paginate page: params[:page],
+        per_page: Settings.length.page
+      redirect_to user_path current_user
+    end
   end
 
   def about
