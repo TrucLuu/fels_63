91b4179e45f5f40b8a56652f5e1dcd9963eba6bf
michalplachyFramgia <michal.plachy@framgia.com>
2015-06-30 14:25:29 +0700

    all

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,14 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      remember user
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_to user
+      if user.active?
+        log_in user
+        params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        flash[:warning] = t "check_email"
+        redirect_to root_url
+      end
     else
       flash[:error] = t "login.failed"
       render "new"
